25bd6e813b7d34a4958e753479a59f326c8fe514
k1mdev <eamonkon@gmail.com>
2025-11-08 16:43:34 -0500

    WIP sidebar collapsibility

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Hero from '../components/Hero'
-import Sidebar from '../components/Sidebar'
+import MainLayout from "@/layouts/MainLayout";
+
 
 const Home = () => {
+
+
   return (
-    // <div className='flex flex-col'>
-    //     <Banner />
-    //     <Hero />
-    // </div>
-    <div className='flex'>
-      <span className='flex-none'>
-        <Sidebar />
-      </span>
-      <span className='flex-1'>
-        <Banner />
-        <Hero />
-      </span>
-    </div>
+
+    <MainLayout>
+      <Banner />
+      <Hero />
+    </MainLayout>
+
   )
 }
 
