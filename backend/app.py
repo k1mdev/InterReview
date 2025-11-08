5b86e535fb883cae97871dead52cb7a1753b43bd
robert winfield <winfieldrl@gmail.com>
2025-11-08 01:33:30 -0500

    Add attempt post route and DB insert attempt

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, g, request
 
 app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_connection(e):
+    db_conn = g.pop('db_conn', None)
+    if db_conn is not None:
+        db_conn.close()
 
 @app.route('/')
 def home():
     return "Hello, Flask!"
+
+@app.route('/api/attempt', methods=['POST'])
+def upload_recording():
+    # file = request.files.get('audio')
+    ...
+    # verify file is mp3, wav, etc.
+    # send to gemini
+    # perform additional analysis?
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/services/db_service.py
+++ backend/services/db_service.py
@@ -1,20 +1,32 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+from flask import g
 
-load_dotenv()
 
-conn = psycopg2.connect(
-    host=os.getenv('INTERREVIEW_DB_ENDPOINT'),
-    port=5432,
-    database="postgres",
-    user=os.getenv('INTERREVIEW_DB_USER'),
-    password=os.getenv('INTERREVIEW_DB_PASSWORD')
-)
+def get_connection():
+    load_dotenv()
+    if 'db_conn' not in g:
+        g.db_conn = psycopg2.connect(
+            host=os.getenv('INTERREVIEW_DB_ENDPOINT'),
+            port=5432,
+            database="postgres",
+            user=os.getenv('INTERREVIEW_DB_USER'),
+            password=os.getenv('INTERREVIEW_DB_PASSWORD')
+        )
+    return g.db_conn
 
-cur = conn.cursor()
-cur.execute("SELECT version();")
-print("Connected to:", cur.fetchone())
-
-cur.close()
-conn.close()
+def post_attempt(data):
+    conn = get_connection()
+    with conn.cursor() as cur:
+        question = data.question
+        answer = data.answer
+        feedback = data.feedback
+        s3_url = data.s3_url
+        uid = data.uid
+        created = data.created
+        query = """
+            INSERT INTO Attempt (question, answer, feedback, uid, created, s3_url) VALUES (%s, %s, %s, %s, %s, %s);
+        """
+        cur.execute(query, (question, answer, feedback, s3_url, uid, created))
+    conn.commit()
