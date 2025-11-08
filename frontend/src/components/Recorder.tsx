0a857381677fed60ef86692f8df90b72858f6304
Takuto Ban <takutoban22@gmail.com>
2025-11-08 16:35:42 -0500

    improve recording page

--- frontend/src/components/Recorder.tsx
+++ frontend/src/components/Recorder.tsx
@@ -2,47 +2,46 @@ import React, { useRef, useState } from 'react';
 import { IconMicrophone, IconPlayerStop } from '@tabler/icons-react';
 import { Button } from '@/components/ui/button'
 
+const padnum = (x: number, space: number) => String(x).padStart(space, "0");
 
 export default function Recorder() {
-  const [isRecording, setIsRecording] = useState<boolean>(false);
-  const [recordedURL, setRecordedURL] = useState<string>('');
-  const [seconds, setSeconds] = useState<number>(0);
+  const [isRecording, setIsRecording] = useState(false);
+  const [recordedURL, setRecordedURL] = useState('');
+  const [seconds, setSeconds] = useState(0);
 
-  const mediaStream = useRef<MediaStream | null>(null);
-  const mediaRecorder = useRef<MediaRecorder | null>(null);
-  const chunks = useRef<Blob[]>([]);
+  const mediaStream = useRef(null);
+  const mediaRecorder = useRef(null);
+  const chunks = useRef([]);
 
   const startRecording = async () => {
+    setRecordedURL('');
     setIsRecording(true);
-    try {
-      setSeconds(0);
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      mediaStream.current = stream;
-      mediaRecorder.current = new MediaRecorder(stream);
+    setSeconds(0);
 
-      mediaRecorder.current.ondataavailable = (e: BlobEvent) => {
-        if (e.data.size > 0) {
-          chunks.current.push(e.data);
-        }
-      };
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaStream.current = stream;
+    mediaRecorder.current = new MediaRecorder(stream);
 
-      const timer = setInterval(() => {
-        setSeconds(prev => prev + 1);
-      }, 1000);
+    mediaRecorder.current.ondataavailable = (e: BlobEvent) => {
+      if (e.data.size > 0) {
+        chunks.current.push(e.data);
+      }
+    };
 
-      mediaRecorder.current.onstop = () => {
-        const recordedBlob = new Blob(chunks.current, { type: 'audio/mp3' });
-        const url = URL.createObjectURL(recordedBlob);
-        setRecordedURL(url);
+    const timer = setInterval(() => {
+      setSeconds(prev => prev + 1);
+    }, 10);
 
-        chunks.current = [];
-        clearInterval(timer);
-      };
+    mediaRecorder.current.onstop = () => {
+      const recordedBlob = new Blob(chunks.current, { type: 'audio/mp3' });
+      const url = URL.createObjectURL(recordedBlob);
+      setRecordedURL(url);
 
-      mediaRecorder.current.start();
-    } catch (error) {
-      console.error(error);
-    }
+      chunks.current = [];
+      clearInterval(timer);
+    };
+
+    mediaRecorder.current.start();
   };
 
   const stopRecording = () => {
@@ -53,27 +52,28 @@ export default function Recorder() {
     }
   };
 
-
-
   return (
     <div>
 
       {isRecording ? (
-        <Button
-          size="icon"
-          variant="secondary"
-          onClick={stopRecording}
-        >
-          <IconPlayerStop />
-        </Button>
+        <div>
+          <div className='text-5xl font-mono'>
+            {`${padnum(Math.floor(seconds / 6000), 2)}:${padnum(Math.floor(seconds / 100 % 60), 2)}:${padnum(seconds % 100, 2)}`}
+          </div>
+          <Button
+            variant="secondary"
+            onClick={stopRecording}
+          >
+            <IconPlayerStop /> Stop Recording
+          </Button>
+        </div>
       ) : (
         <div className='flex items-center'>
-          <span className="text-sm p-1">Record Your Response</span>
           <Button
-            size="icon"
             variant="secondary"
             onClick={startRecording}
           >
+            {recordedURL ? 'Re-' : ''}Record Your Response
             <IconMicrophone />
           </Button>
         </div>
@@ -82,7 +82,7 @@ export default function Recorder() {
       {recordedURL && (
         <>
           <audio className='p-2' controls src={recordedURL} />
-          <Button size="icon" variant="secondary">
+          <Button variant="secondary">
             Submit
           </Button>
         </>
