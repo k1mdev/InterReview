000b15ef90942b662eacdc8c5df2cd7f3d7a8a21
k1mdev <eamonkon@gmail.com>
2025-11-07 22:33:13 -0500

    Basic frontend changes

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Banner'
+
+const Home = () => {
+  return (
+    <>
+        <Banner />
+        <div>Home Page</div>
+    </>
+    
+  )
+}
+
+export default Home
