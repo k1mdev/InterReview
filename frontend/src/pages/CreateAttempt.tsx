c752932a9965b033dc6be271c6259a3cef21d8e3
k1mdev <eamonkon@gmail.com>
2025-11-09 06:07:33 -0500

    Fix question box styling

--- frontend/src/pages/CreateAttempt.tsx
+++ frontend/src/pages/CreateAttempt.tsx
@@ -11,8 +11,13 @@ const CreateAttempt = () => {
       <MainLayout>
         <div className="flex flex-col items-center justify-center h-screen">
           <span className='absolute bottom-2/3 flex flex-col items-center'>
-            <textarea className='rounded-2xl border-2 w-[45vw] h-[13vh] p-7  text-2xl resize-none' placeholder="What's the question?" value={title} onChange={(event) => setTitle(event.target.value)} />
-          </span>
+          <textarea
+            className='rounded-2xl border-2 w-[45vw] h-[13vh] p-4 text-2xl resize-none'
+            placeholder="What's the question?"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </span>
           <Recorder title={title} />
         </div>
       </MainLayout>
